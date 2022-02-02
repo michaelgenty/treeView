065ddc5bfa965e28a662ad9dff05f1d0e98bf9d4
Michael <michael.genty@outlook.fr>
2022-02-02 12:04:48 -0500

    Implement the syncfusion file manager

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # treeView
+
+## This project uses angular and the file manager solution from syncfusion
+
+
+Steps to follow:
+
+1/ 
+```bash
+npm install
+```
+
+2/
+```bash
+cd node_modules\@syncfusion\ej2-filemanager-node-filesystem
+```
+```bash
+npm install
+```
+
+3/
+```bash
+npm run explore -- C:/yourFolderPath
+```
+
+
+4/
+```bash
+ng serve
+```
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'treeView';
+
+export class AppComponent implements OnInit{
+  public ajaxSettings: any;
+  public hostUrl: string = 'http://localhost:8090/';
+
+  ngOnInit(): void {
+    // Initializing the File Manager with NodeJS service.
+    this.ajaxSettings = {
+      url: this.hostUrl,
+      downloadUrl: this.hostUrl+ "Download",
+      uploadUrl: this.hostUrl+ "Upload",
+      getImageUrl: this.hostUrl+ "GetImage"
+    };
+  }
 }
